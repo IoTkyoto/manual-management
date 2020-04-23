4cbae4cf2229cb206d9c2ba28695317a629efff5
sunagawa <naoko_sunagawa@kyoso.co.jp>
2020-04-23 14:40:54 +0900

    chore-fix: codecommitからリポジトリをクローンして作業するよう修正

---
+++ build-scripts/git-clone.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+timeout=30
+command="aws configure --profile codecommit"
+access_key=$CODEBUILD_ACCESS_KEY
+secret_key=$CODEBUILD_SECRET_KEY
+codecommit_url=$CODEBUILD_CODECOMMIT_URL
+
+expect -c "
+    set timeout ${timeout}
+    spawn ${command}
+    expect \"Acccess\"
+    send \"${access_key}\n\"
+    expect \"Secret\"
+    send \"${secret_key}\n\"
+    expect \"$\"
+    exit 0
+"
+
+git config --global credential.helper "!aws codecommit --profile codecommit credential-helper $@"
+git config --global credential.UseHttpPath true
+
+git clone ${codecommit_url}

6f8767bcb96d434fd984eb630dc0c42ab366aca5
sunagawa <naoko_sunagawa@kyoso.co.jp>
2020-04-23 16:15:30 +0900

    chore-fix: cloneの手順をもとに戻す

--- build-scripts/git-clone.sh
+++ build-scripts/git-clone.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 timeout=30
-command="aws configure"
+command="aws configure --profile codecommit"
 access_key=$ACCESS_KEY
 secret_key=$SECRET_KEY
 codecommit_url=$CODECOMMIT_URL
@@ -21,5 +21,8 @@ expect -c "
     exit 0
 "
 
+git config --global credential.helper "!aws codecommit --profile codecommit credential-helper $@"
+git config --global credential.UseHttpPath true
 echo $codecommit_url
+echo "clone repository"
 git clone $codecommit_url
